be0f48b8f1ee17b326a260e39799149833da08a2
ffeldhaus <florian.feldhaus@gmail.com>
2018-02-11 22:08:53 +0100

    Changed to styleUrls and removed loader configuration for SASS

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,3 @@
 const environment = require('./environment')
-const merge = require('webpack-merge')
-const customConfig = require('./custom')
 
 module.exports = environment.toWebpackConfig()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,18 +1,5 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 
-environment.loaders.set('style', {
-test: /\.(scss|sass|css)$/,
-    use: [{
-        loader: "to-string-loader"
-    }, {
-        loader: "css-loader"
-    }, {
-        loader: "resolve-url-loader"
-    }, {
-        loader: "sass-loader"
-    }]
-})
-
 environment.loaders.append('typescript', typescript)
 module.exports = environment
